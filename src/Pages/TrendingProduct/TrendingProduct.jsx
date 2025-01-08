b84250fe510e248476cd564473a4e70186751eb3
suphal-dev <suphal@klizos.com>
2025-01-08 19:31:40 +0530

    gap bug fix and images

--- src/Pages/TrendingProduct/TrendingProduct.jsx
+++ src/Pages/TrendingProduct/TrendingProduct.jsx
@@ -74,10 +74,10 @@ const TrendingProduct = () => {
 
          <div className="flex flex-col ">
             <h1 className='trending_page_heading' style={{color:"black",paddingLeft:"6px"}}>Featured Products </h1>
-            <div className="grid grid-cols-1 sm:border sm:border-gray-200  sm:grid-cols-[70%_30%] sm:gap-2 ">
+            <div className="grid grid-cols-1  sm:grid-cols-[66%_34%]  ">
                 <div className="trending_grid_1  ">
                     <img  className='trending_page_img1' src="watch1.png" alt="" />
-                    <div className="grid grid-rows-[40px_1fr]">
+                    <div className="grid grid-rows-[40px_1fr] bg-gray-100">
                         <p className='trending_page_para'>Spotlight</p>
                         <h1 className="trending_page_heading" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique , Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique   </h1>
                        
@@ -85,7 +85,7 @@ const TrendingProduct = () => {
                    
                 </div> 
 
-                <div  className=' grid grid-rows-2 sm:mx-4 '>
+                <div  className=' grid  grid-rows-2 sm:ml-4 sm:mr-1.5 '>
                     <div className="" ><img style={{height:"100%",width:"100%"}} src="watch3.png" alt="" /></div>
                     <div className=""><img style={{height:"100%",width:"100%"}} src="watch4.png" alt="" /></div>
                    
@@ -100,7 +100,40 @@ const TrendingProduct = () => {
 
          </div>
 
+
+
+
        {/* shop by brand section */}
+
+
+
+       <div className="shop_by_brand_outer_box w-full p-4 grid grid-rows-[40px_1fr]  ">
+         <h1 style={{color:"blue"}} className='trending_page_heading '>Shop By Brand</h1>
+         <div className="shop_by_brand_inner_box grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7  ">
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="logo.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="alpina.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="angelus.png" alt="" /></div>
+            
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="arnold.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="baume.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="bell.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img style={{height:"80px",width:"95%"}} src="bianchet.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="logo.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="alpina.png" alt="" /></div>
+            <div className="flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="angelus.png" alt="" /></div>
+           
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="arnold.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="baume.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="bell.png" alt="" /></div>
+            <div className=" flex justify-center bg-gray-100"> <img  style={{height:"80px",width:"95%"}} src="bianchet.png" alt="" /></div>
+
+         </div>
+
+       </div>
+
+        
+
+       {/* App section */}
 
         
         <SectionApp/>
