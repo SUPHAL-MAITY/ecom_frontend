ab933c45efbcc8ca740d31054ef890dbf4c5592e
suphal-dev <suphal@klizos.com>
2025-01-29 19:52:13 +0530

    add category with validation

--- src/Pages/AddCategory/AddCategory.jsx
+++ src/Pages/AddCategory/AddCategory.jsx
@@ -1,80 +1,92 @@
 import { useState } from "react";
 import SidebarForAdmin from "../../Components/SidebarForAdmin/SidebarForAdmin";
-import axios from "axios"
+import axios from "axios";
 import "./AddCategory.css";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
 export default function AddCategory() {
   let [name, setName] = useState("");
-  
-  
-  const my_api=import.meta.env.VITE_API_BASE_URL
-  const handleSubmit =async(e) => {
-    e.preventDefault();
-    if (name.trim() === ""){
-        toast.error("please  fill the input")
-        setName("")
-        return
-    };
-    try{
-         name=name.trim()
-        const {data}=await axios.post(`${my_api}/create-category`,{name})
-        setName("")
-        console.log("data",data)
-        toast("category created successfully!")
-    }catch(err){
-        console.log(err)
-        if(err.response.status==409){
-            setName("")
-            toast(`${name} brand  already exists`)
-        }
+  const [image, setImage] = useState("");
 
+  const my_api = import.meta.env.VITE_API_BASE_URL;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name.trim() === "") {
+      toast.error("please  fill the input");
+      setName("");
+      return;
     }
-    
+    try {
+      name = name.trim();
+      console.log(image);
+      const { data } = await axios.post(
+        `${my_api}/create-category`,
+        { name, image },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setName("");
+      setImage("");
+      console.log("data", data);
+      toast("category created successfully!");
+    } catch (err) {
+      console.log(err);
+      ///validating for existance of same name
+      if (err.response.status == 409) {
+        setName("");
+        toast(`${name} brand  already exists`);
+      }
+    }
   };
 
   return (
-
     <>
+      <div className="app-container2">
+        <div className=" w-[250px]  ">
+          <SidebarForAdmin />
+        </div>
 
-<div className="app-container2">
+        <div className="main-content2  flex items-center justify-center min-h-screen  bg-[url(fossil.png)] bg-cover bg-center ">
+          <ToastContainer />
 
-        
-      
-<div className=" w-[250px]  ">
-      <SidebarForAdmin/>
+          <div className=" bg-white/30 backdrop-blur-xs border border-white/20 p-6 rounded-lg shadow-lg w-96">
+            <h2 className="text-xl font-bold text-white font-serif mb-4">
+              Add a Brand Name
+            </h2>
 
-</div>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 font-serif"
+                placeholder="Enter brand name"
+              />
+              <input
+                type="file"
+                name="image"
+                onChange={(e) => setImage(e.target.files[0])}
+                className="w-full mt-2 p-2 bg-white border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Upload Image"
+                accept=".jpg, .jpeg, .png"
+                required
+              />
 
-
-<div className="main-content2  flex items-center justify-center min-h-screen  bg-[url(fossil.png)] bg-cover bg-center ">
-<ToastContainer /> 
-  
-  <div className=" bg-white/30 backdrop-blur-xs border border-white/20 p-6 rounded-lg shadow-lg w-96">
-    <h2 className="text-xl font-bold text-white font-serif mb-4">Add a Brand Name</h2>
-    
-      <form onSubmit={handleSubmit}>
-        
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 font-serif"
-          placeholder="Enter brand name"
-        />
-        <button
-          type="submit"
-          className="mt-4 w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition"
-        >
-          Submit
-        </button>
-      </form>
-    
-  </div>
-</div>
-    
-</div>  
+              <button
+                type="submit"
+                className="mt-4 w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-   
   );
 }
