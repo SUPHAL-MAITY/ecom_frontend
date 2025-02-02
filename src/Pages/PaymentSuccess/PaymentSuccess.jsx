3588aeace6523732a3f2ac54cdca9296acf36713
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-03 00:08:14 +0530

    order saved in db after payment

--- src/Pages/PaymentSuccess/PaymentSuccess.jsx
+++ src/Pages/PaymentSuccess/PaymentSuccess.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React ,{ useEffect } from 'react'
 import "./PaymentSuccess.css"
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+
 
 import { useWindowSize } from 'react-use'
 import Confetti from 'react-confetti'
@@ -9,12 +12,46 @@ import Confetti from 'react-confetti'
 const PaymentSuccess = () => {
 
     const { width, height } = useWindowSize()
+    const [searchParams] = useSearchParams();
+    const sessionId = searchParams.get("session_id");
+
+   const apiUrl=import.meta.env.VITE_API_BASE_URL
+   console.log(sessionId)
+
+  useEffect(() => {
+    if (sessionId) {
+      verifyPayment(sessionId);
+    }
+  }, [sessionId]);
 
 
 
+  const verifyPayment = async (sessionId) => {
+    try {
+      const { data } = await axios.get( `${apiUrl}/verify-payment?session_id=${sessionId}`);
+
+      if (data.payment_status === "paid") {
+        console.log("✅ Payment verified! Saving order...");
+
+        // Save order details in your database
+        // await axios.post("http://localhost:5000/api/v1/save-order", {
+        //   userDetails: data.userDetails,
+        //   items: data.cartItems,
+        //   totalAmount: data.amount_total / 100,
+        //   paymentIntent: data.payment_intent,
+        //   status: "Paid",
+        // });
+        alert("order saved sucessfully")
+
+        console.log("✅ Order saved successfully!");
+      }
+    } catch (error) {
+      console.error("❌ Error verifying payment:", error.message);
+    }
+  };
     
 
-
+ 
 
 
 
