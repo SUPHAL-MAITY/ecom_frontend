6b411d4ed7a9d2bf5dc833c05f272eb859cd6a02
suphal-dev <suphal@klizos.com>
2025-02-04 19:05:45 +0530

    bg change of add category

--- src/Pages/AddCategory/AddCategory.jsx
+++ src/Pages/AddCategory/AddCategory.jsx
@@ -78,7 +78,7 @@ export default function AddCategory() {
           <SidebarForAdmin />
         </div>
 
-        <div className="main-content2  flex items-center justify-center min-h-screen   bg-[url(fossil.png)] invert-20  bg-cover bg-center  ">
+        <div className="main-content2  flex items-center justify-center min-h-screen bg-black    invert-20  bg-cover bg-center  ">
         
          
           <ToastContainer />
