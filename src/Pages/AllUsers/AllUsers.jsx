286db3d65ca48b9ea3862ff43a5ca763d599f25b
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-15 00:22:13 +0530

    all users modified

--- src/Pages/AllUsers/AllUsers.jsx
+++ src/Pages/AllUsers/AllUsers.jsx
@@ -1,11 +1,17 @@
 
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import SidebarForAdmin from '../../Components/SidebarForAdmin/SidebarForAdmin'
-
+import axios from "axios"
 
 const AllUsers = () => {
 
     const [searchQuery, setSearchQuery] = useState('')
+
+    const [page,setPage]=useState(1)
+    const [loading, setLoading] = useState(false);
+    const [totalPages,setTotalPages]=useState()
+
+    const [users,setUsers]=useState([])
 
     const orders = [
         { id: '#XGY-346', created: '7 min ago', customer: 'Albert Flores', total: '$630.00', profit: '$86.70', status: 'Pending' },
@@ -16,6 +22,55 @@ const AllUsers = () => {
         { id: '#SKP-035', created: '2 day ago', customer: 'Eleanor Pena', total: '$290.00', profit: '$29.00', status: 'Rejected' },
         { id: '#SKP-567', created: '7 min ago', customer: 'Dan Wilson', total: '$590.00', profit: '$50.00', status: 'Shipped' },
       ]
+
+
+
+      useEffect(() => {    
+        fetchData();      
+    
+  }, [page]);
+  
+
+///data fetching for all products with pagination
+
+ const fetchData = async () => {
+   try {
+     setLoading(true);
+     const { data } = await axios.get(
+       `http://localhost:3000/api/v1/get-all-users?page=${page}`,{withCredentials:true}
+     );
+     setUsers(data?.data.users)
+     setTotalPages(data?.data?.totalPages)
+     setLoading(false);
+
+     console.log("all users data",data);
+   } catch (error) {
+     console.log(error);
+     setLoading(false);
+   }
+ };
+
+  
+//for pagination purpose
+ const  increasePage=()=>{
+   if(page==totalPages) return;
+   
+   setPage(prev=>prev+1)
+ }
+
+ 
+
+const decreasePage=()=>{
+ if(page==1) return;
+ setPage(prev=>prev-1)
+}
+
+
+
+
+
+
+
     
 
 
@@ -23,7 +78,7 @@ const AllUsers = () => {
     <>   
 
   
-    <div className="flex min-h-screen items-stretch">
+    <div className="flex min-h-screen items-stretch font-serif">
      <div className="h-full">
          <SidebarForAdmin/>
  
@@ -92,33 +147,32 @@ const AllUsers = () => {
              <table className="w-full">
                  <thead>
                  <tr className="border-b bg-gray-200">
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">ORDER ID</th>
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">CREATED</th>
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">CUSTOMER</th>
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">TOTAL</th>
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">PROFIT</th>
-                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">STATUS</th>
+                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">Name</th>
+                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">Email</th>
+                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">Phone</th>
+                    
+                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">Profile</th>
+                     <th className="text-left py-4 px-4 text-sm font-medium text-gray-500">Role</th>
                      <th className="w-10"></th>
                  </tr>
                  </thead>
                  <tbody>
-                 {orders.map((order,i) => (
-                     <tr key={order.id} className= {`border-b ${ i%2 !==0 ? "bg-gray-50" : "bg-white-100" } `}>
-                     <td className="py-4 px-4 text-sm font-medium text-gray-900">{order.id}</td>
-                     <td className="py-4 px-4 text-sm text-gray-500">{order.created}</td>
-                     <td className="py-4 px-4 text-sm text-gray-900">{order.customer}</td>
-                     <td className="py-4 px-4 text-sm text-gray-900">{order.total}</td>
-                     <td className="py-4 px-4 text-sm text-gray-900">{order.profit}</td>
-                     <td className="py-4 px-4">
+                 {users.map((user,i) => (
+                     <tr key={user._id} className= {`border-b ${ i%2 !==0 ? "bg-gray-50" : "bg-white-100" } `}>
+                     <td className="py-3 px-4 text-sm font-medium text-gray-900">{user.name}</td>
+                     <td className="py-3 px-4 text-sm text-gray-500">{user.email}</td>
+                     <td className="py-3 px-4 text-sm text-gray-900">{user.phone}</td>
+                     
+                     <td className="py-3 px-4 text-sm text-gray-900"><img src={user.profileUrl} alt="" className='w-[50px] h-[50px]' /></td>
+                     <td className="py-3 px-4">
                          <span
-                         className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium
-                             ${order.status === 'Pending' ? 'bg-yellow-100 text-yellow-800' : ''}
-                             ${order.status === 'Confirmed' ? 'bg-blue-100 text-blue-800' : ''}
-                             ${order.status === 'Shipped' ? 'bg-green-100 text-green-800' : ''}
-                             ${order.status === 'Rejected' ? 'bg-red-100 text-red-800' : ''}
+                         className={`inline-flex items-center px-4 py-4 rounded-full text-xs font-medium
+                             ${user.role === 'admin' ? 'bg-red-100 text-yellow-800 ' : ''}
+                             ${user.role === 'customer' ? 'bg-blue-100 text-blue-800 ' : ''}
+                             
                          `}
                          >
-                         {order.status}
+                         {user.role}
                          </span>
                      </td>
                      <td className="py-4 px-4">
@@ -136,11 +190,19 @@ const AllUsers = () => {
      </div>
  
  
+    <div className="flex justify-center  space-x-1 mt-2">
+      <button disabled={page==1} onClick={decreasePage} className={`min-w-9 rounded-md bg-blue-100  border border border-slate-300 py-2 px-3 text-black font-serif font-bold text-center text-sm transition-all  text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800  active:border-slate-800 active:text-white active:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2`}>
+            Prev
+          </button>
+          
+          <button disabled={page==totalPages} onClick={increasePage} className={`min-w-9 rounded-md bg-blue-100  border border border-slate-300 py-2 px-3 text-black font-serif font-bold text-center text-sm transition-all  text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800  active:border-slate-800 active:text-white active:bg-blue-200 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2`}>
+            Next
+          </button>
+    </div>
+
+
  
      </div>
-    
- 
- 
  
  
     </div>
