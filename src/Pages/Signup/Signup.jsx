2737dac6aad405712e3d476d394fcd0ad025548b
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-23 21:22:40 +0530

    bg in sign up changed

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -318,7 +318,7 @@ useEffect(()=>{
                 name="confirmPassword"
                 className= {`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
                   focus:ring-blue-500 focus:border-blue-500 block w-full p-2 
-                  dark:bg-gray-700 dark:border-gray-600 
+                  
                   ${formik.touched.confirmPassword && formik.errors.confirmPassword ? "" : " mb-4 2xl:mb-10"}`}
                 placeholder="•••••••••"
                 
