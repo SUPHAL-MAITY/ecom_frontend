fe2f6671603a0c299a281bf9dd02e220dc0a7093
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-02-23 21:16:50 +0530

    sign up page form color fix

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -223,7 +223,7 @@ useEffect(()=>{
                 <input
                   type="text"
                   id="address2"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 "
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2  "
                   placeholder=""
                   value={address2}
                   onChange={(e)=>setAddress2(e.target.value)}
@@ -293,7 +293,7 @@ useEffect(()=>{
               <input
                 type="password"
                 id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 "
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2   "
                 placeholder="•••••••••"
                 name="password"
                 value={formik.values.password}
